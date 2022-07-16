b8d9556abcc85610f62c41c34c3571ccfaca4cce
wiF0n <david.kubanda@gmail.com>
2022-07-16 16:15:26 +0200

    Add constants file

--- constants.py
+++ constants.py
@@ -5,17 +5,63 @@ Author: David Kubanda
 Date: 2022-07-16
 """
 
+# Data related constants
 DATA_PTH = r"./data/bank_data.csv"
 
-cat_columns_list = [
+cat_cols_lst = [
     'Gender', 'Education_Level', 'Marital_Status', 'Income_Category',
     'Card_Category'
 ]
 
-quant_columns_list = [
+quant_cols_lst = [
     'Customer_Age', 'Dependent_count', 'Months_on_book',
     'Total_Relationship_Count', 'Months_Inactive_12_mon',
     'Contacts_Count_12_mon', 'Credit_Limit', 'Total_Revolving_Bal',
     'Avg_Open_To_Buy', 'Total_Amt_Chng_Q4_Q1', 'Total_Trans_Amt',
     'Total_Trans_Ct', 'Total_Ct_Chng_Q4_Q1', 'Avg_Utilization_Ratio'
 ]
+
+modeling_cols_lst = [
+    'Customer_Age', 'Dependent_count', 'Months_on_book',
+    'Total_Relationship_Count', 'Months_Inactive_12_mon',
+    'Contacts_Count_12_mon', 'Credit_Limit', 'Total_Revolving_Bal',
+    'Avg_Open_To_Buy', 'Total_Amt_Chng_Q4_Q1', 'Total_Trans_Amt',
+    'Total_Trans_Ct', 'Total_Ct_Chng_Q4_Q1', 'Avg_Utilization_Ratio',
+    'Gender_Churn', 'Education_Level_Churn', 'Marital_Status_Churn',
+    'Income_Category_Churn', 'Card_Category_Churn'
+]
+
+RAW_TARGET_COL = "Attrition_Flag"
+TARGET_COL = "Churn"
+
+# EDA related constants
+
+EDA_IMG_FLDR_PTH = "./images/eda/"
+
+# Modeling related constants
+
+model_arch_lst = ["rf", "lr"]
+
+TEST_SIZE = 0.3
+RANDOM_STATE = 42
+
+LR_SOLVER = "lbfgs"
+LR_MAX_ITER = 3000
+
+rf_param_grid_dict = {
+    'n_estimators': [200, 500],
+    'max_features': ['auto', 'sqrt'],
+    'max_depth': [4, 5, 100],
+    'criterion': ['gini', 'entropy']
+}
+
+RF_NUM_CV_FOLDS = 5
+
+RSLT_IMG_FLDR_PTH = "./images/results/"
+RF_MODEL_PTH = './models/rfc_model.pkl'
+LR_MODEL_PTH = './models/logistic_model.pkl'
+ROC_IMG_PTH = "./images/results/test_au_roc.jpg"
+FI_IMG_PTH = "./images/results/feat_imp.jpg"
+
+CLS_RPRT_FNT = "monospace"
+CLS_RPRT_FNT_SIZE = 10
